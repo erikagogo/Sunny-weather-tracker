9b2bf2153ef928d65a3cd03fd20221fbb0c6e9ac
erikagogo <erika.gogo@gmail.com>
2025-07-11 14:00:45 -0400

    Added axios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-function search(event) {
+function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("city");
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("Submit", search);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
