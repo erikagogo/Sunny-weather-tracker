b41246f5a323b62c68ec035298023d3d05003a33
erikagogo <erika.gogo@gmail.com>
2025-07-11 22:21:55 -0400

    Included more weather data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 function refreshWeather(response) {
-  let temperatureElement = document.querySelector("temperature");
+  let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  description.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  speedElement.innerHTML = `${response.data.wind.speed}km/hr`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function searchCity(city) {
-  let apiKey = "fcb5b04c3b01oef210621t20e3aeda2f";
+  let apikey = "fcb5b04c3b01oef210621t20e3aeda2f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
@@ -16,6 +41,7 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
+
   cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
@@ -23,4 +49,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-let apikey = "fcb5b04c3b01oef210621t20e3aeda2f";
+searchCity("Nairobi");
